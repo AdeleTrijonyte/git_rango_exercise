0e771ad9ffa189c02eb1b068afefea9346512d15
Adele Trijonyte <2288630T@student.gla.ac.uk>
2017-01-26 01:04:42 +0000

    chapter 4.3 and the exercises

--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -15,5 +15,8 @@ def index(request):
 # return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
 	
 def about(request):
-	return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+	return render(request, 'rango/about.html')
+
+# previous version
+# return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
 	
