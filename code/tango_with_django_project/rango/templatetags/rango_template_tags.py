1991fc887c9a9ac02c1805c44cbe1802a89c5ff3
Adele Trijonyte <2288630T@student.gla.ac.uk>
2017-02-13 16:56:27 +0000

    chapter 8.5

--- code/tango_with_django_project/rango/templatetags/rango_template_tags.py
+++ code/tango_with_django_project/rango/templatetags/rango_template_tags.py
@@ -4,7 +4,7 @@ from rango.models import Category
 register = template.Library()
 
 @register.inclusion_tag('rango/cats.html')
-def get_category_list():
+def get_category_list(cat=None):
     return {'cats': Category.objects.all(),
-            'act_cat': cat}
+        'act_cat': cat}
 # cat parameter added - parameterised template tags
