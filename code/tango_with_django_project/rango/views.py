427e08ec9be67a39b7470e47bdd35380706bd1c7
Adele Trijonyte <2288630T@student.gla.ac.uk>
2017-02-15 00:30:59 +0000

    chapter 10.5 - visits cookie seems to work

--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -16,9 +16,35 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
+from datetime import datetime
+
 def index(request):
 
     request.session.set_test_cookie()
+
+    def visitor_cookie_handler(request, response):
+        # Get the number of visits to the site.
+        # We use the COOKIES.get() function to obtain the visits cookie.
+        # If the cookie exists, the value returned is casted to an integer.
+        # If the cookie doesn't exist, then the default value of 1 is used.
+        visits = int(request.COOKIES.get('visits', '1'))
+
+        last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+        last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                            '%Y-%m-%d %H:%M:%S')
+
+        # If it's been more than a day since the last visit...
+        if (datetime.now() - last_visit_time).days > 0:
+            visits = visits + 1
+            #update the last visit cookie now that we have updated the count
+            response.set_cookie('last_visit', str(datetime.now()))
+        else:
+            visits = 1
+            # set the last visit cookie
+            response.set_cookie('last_visit', last_visit_cookie)
+
+        # Update/set the visits cookie
+        response.set_cookie('visits', visits)
 
     # Query the database for a list of ALL categories currently stored.
     # Order the categories by no. likes in descending order.
@@ -32,7 +58,13 @@ def index(request):
     context_dict = {'categories': category_list, 'pages': page_list}
 
     # Render the response and send it back!
-    return render(request, 'rango/index.html', context_dict)
+    # Obtain our Response object early so we can add cookie information.
+    response = render(request, 'rango/index.html', context_dict)
+
+    # Call function to handle the cookies
+    visitor_cookie_handler(request, response)
+    # Return response back to the user, updating any cookies that need changed.
+    return response
 
 # before chapter 6:
     # Construct a dictionary to pass to the template engine as its context.
